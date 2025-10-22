6c3196fc1f5c58c09ffc3cbab3256001bc9f598e
slieberman27 <slieberman@browning.edu>
2025-10-22 13:28:07 +0000

    added else/error statement

--- helloWorld10-22.py
+++ helloWorld10-22.py
@@ -1,12 +1,12 @@
 print("hello world");
 #prints hello world to console
 
-x = 9;
+x = 10;
 y = "red";
 #declaring variable values
 
 if x == 10:
     print(x , y);
-else
+else:
     print("ts variable x did NOT equal 10 *pray* *cry* *wilting rose*");
 #should print "10 red" if x = 10, and prints an error if it does not
